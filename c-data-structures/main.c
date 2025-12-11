2510e1338c93009455eb66611e0ee92a97db8758
jhrchicken <122204170+jhrchicken@users.noreply.github.com>
2025-12-11 19:54:14 +0900

    Initial Commit

---
+++ c-data-structures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-data-structures
+//
+//  Created by 하림 on 12/11/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
