8f95c7a34ce17c82fdfe5cc870f357fc549f910b
jhrchicken <jhrchicken@users.noreply.github.com>
2026-02-07 19:34:30 +0900

    삽입 정렬하기

--- c-data-structures/quickSort1.c
+++ c-data-structures/quickSort1.c
@@ -17,7 +17,7 @@ int main(void) {
     int i, list[8] = { 69, 10, 30, 2, 16, 8, 31, 22 };
     int size = sizeof(list) / sizeof(list[0]);
     
-    // 정렬 전의 원소 출력
+    x
     printf("[정렬할 원소]\n\t");
     for (i = 0; i < size; i++) {
         printf("%3d ", list[i]);
